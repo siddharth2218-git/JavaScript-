6de118f296c23d09251a08b8cdd8a074c81c1f03
siddharth2218-git <siddharthpalanisamy22@gmail.com>
2024-07-12 09:25:43 +0530

    Ex 1 Completed Sucessfully

--- JavaScript_Programming_Exercises-master/01_JS_Basics/Ch1_pgm_01.js
+++ JavaScript_Programming_Exercises-master/01_JS_Basics/Ch1_pgm_01.js
@@ -5,14 +5,17 @@ console.log("Hello World!");
 
 /* Further Adventures
  *
- * 1) Change the text between the parentheses.
- *
- * 2) Click Run on the console to run the program again.
+ * 1) Change the text between the parentheses. */
+console.log("My JavaScript Practice!");
+
+ /* 2) Click Run on the console to run the program again.
  *
  * 3) Add your own console.log statement
  *    to the program, so there are two.
- *    Don't forget the semicolon.
- *
+ *    Don't forget the semicolon.*/
+console.log("New data");
+ 
+ /*
  * 4) Click Run.
  *
  */
